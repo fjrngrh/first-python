b7f93a3d940bdbdfabecdbcef9778fc1c03de5fe
fjrngrh <fajarnugraha29@gmail.com>
2022-10-29 07:56:00 +0700

    percabangan

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ ini adalah demo project dengan python
 """
 print ('ibu berkata "pergi ke toko" ')
 print('budi menjawab, "beli apa?"')
-print('ibu , "beli apa?"')
-print('budi menjawab, "beli apa?"')
+print('ibu , "beli susu 1, jika ada telur, beli 6"')
+print('budi pergi ke toko')
+print('budi cek harga dan membeli susu, mungkin juga telur kalau ada')
 
 # Percabangan
+jumlahbotolsusu = 173
+jumlahtelur = 158
+
+print (f"jumlah botol susu {jumlahbotolsusu} btl")
